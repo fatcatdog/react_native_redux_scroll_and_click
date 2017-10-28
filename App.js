ad945da8aa3199abfcb8f3c29b11407d1df30af9
fatcatdog <duchenjacob@gmail.com>
2017-10-28 00:14:20 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers'
+import { Header } from './src/components/common'
+import LibraryList from './src/components/LibraryList'
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
